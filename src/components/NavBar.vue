92a9a5fdbaae0711cce646308cd0b110321bc8af
Sebastian Br <sebastian.brancacho@gmail.com>
2023-11-23 15:21:27 -0500

    init

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+ <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <img class="logoImg" src="../assets/tbps-logo.webp" alt="">
+  </div>
+
+</nav>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+<style scoped>
+  .logoImg{
+    max-width: 130px;
+  }
+</style>
